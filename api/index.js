8b0c1e2e3efa99ff4749f7b8373594fc258b89e4
MohamedMostafa <83365873+mo634@users.noreply.github.com>
2024-01-25 14:20:45 +0200

    connect to mongon DB

--- api/index.js
+++ api/index.js
@@ -1,9 +1,20 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from "dotenv"
+dotenv.config()
 
 const app = express()
 
+// connect DB 
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("connected  to DB "))
+.catch((err) => { console.log("error to connect DB : ",err) })
+
+// use   middleware
 app.use(express.json())
 
+
+// run server 
 app.listen(4000, () => {
     console.log('server is running on port 4000')
 })
