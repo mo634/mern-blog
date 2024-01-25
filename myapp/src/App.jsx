fdfeee41d92eb5f3e6f2f125bbb079da1c835e8f
MohamedMostafa <83365873+mo634@users.noreply.github.com>
2024-01-25 11:15:27 +0200

    create pages and configure routes

--- myapp/src/App.jsx
+++ myapp/src/App.jsx
@@ -1,7 +1,26 @@
-
+import {BrowserRouter ,Routes ,Route} from "react-router-dom"
+import Home from "./pages/Home"
+import Dashboard from "./pages/Dashbaoard"
+import SignIn from "./pages/SignIn"
+import SignUp from "./pages/SignUp"
+import About from "./pages/About"
 const App = () => {
   return (
-    <div>App</div>
+    // config routes with react router
+    <BrowserRouter>
+    
+    <Routes>
+
+    <Route path="/" element = {<Home/>}/>
+
+    <Route path="/dashboard" element = {<Dashboard/>}/>
+    <Route path="/sign-in" element = {<SignIn/>}/>
+    <Route path="/sign-up" element = {<SignUp/>}/>
+    <Route path="/about" element = {<About/>}/>
+
+    </Routes>
+    
+    </BrowserRouter>
   )
 }
 
---
+++ myapp/src/pages/Dashbaoard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Dashbaoard = () => {
+  return (
+    <div>Dashbaoard</div>
+  )
+}
+
+export default Dashbaoard
