deeeefe922b8efc389fe01884077a62a63f52bd9
MohamedMostafa <83365873+mo634@users.noreply.github.com>
2024-02-01 14:28:01 +0200

    complete sidebar of dashboard

---
+++ clinet/src/components/SideBar.jsx
@@ -0,0 +1,56 @@
+import { Sidebar } from 'flowbite-react'
+import { useEffect, useState } from 'react'
+import {HiArrowSmRight, HiUser } from 'react-icons/hi'
+import { Link, useLocation } from 'react-router-dom'
+const SideBar = () => {
+    // states 
+    const location = useLocation() 
+
+    const [tab, setTab] = useState("")
+    useEffect(() => {
+        // get tab value from URL
+
+        const urlParams = new URLSearchParams(location.search)
+
+        const tabParam = urlParams.get("tab")
+
+        setTab(tabParam)
+
+    },[location.search])
+return (
+    <Sidebar className=' h-screen w-full md:w-[20%]' >
+        
+        <Sidebar.Items>
+
+            <Sidebar.ItemGroup>
+
+                {/* render profile item */}
+
+                <Link to={"?tab=profile"}>
+                <Sidebar.Item 
+                label="profile"
+                icon={HiUser}
+                active = {tab==="profile"} 
+                className='cursor-pointer'
+                >
+                    profile
+                </Sidebar.Item>
+
+                </Link>
+                {/* render signOut item */}
+
+                <Sidebar.Item 
+                icon={HiArrowSmRight}
+                className='cursor-pointer'
+                >
+                    signout
+                </Sidebar.Item>
+            
+            </Sidebar.ItemGroup>
+        
+        </Sidebar.Items>
+    </Sidebar>
+)
+}
+
+export default SideBar
--- clinet/src/pages/Dashbaoard.jsx
+++ clinet/src/pages/Dashbaoard.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
-
+import{ useEffect, useState } from 'react'
+import { useLocation } from 'react-router-dom'
+import SideBar from '../components/SideBar'
+import ProfileDashboard from '../components/ProfileDashboard'
 const Dashbaoard = () => {
+  
+  const location = useLocation()
+  
+  const [tab , setTab] = useState("")
+  
+  useEffect(() => {
+    // get tab value from URL 
+
+    const urlParams = new URLSearchParams(location.search)
+
+    const tabPramam = urlParams.get("tab")
+
+    setTab(tabPramam)
+
+
+  },[location.search])
+  
   return (
-    <div>Dashbaoard</div>
+    <div className=' min-h-screen flex flex-col md:flex-row '>
+      {/* render sidebar */}
+
+      <SideBar/>
+
+      {/* render profile dashboard */}
+
+      {
+        tab==="profile" && <ProfileDashboard/>
+      }
+    </div>
   )
 }
 
