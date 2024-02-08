d01cf32956d66561aa071476544366e611bc3d9b
MohamedMostafa <83365873+mo634@users.noreply.github.com>
2024-02-08 13:15:57 +0200

    add get posts api route

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -25,3 +25,58 @@ export const create = async (req, res, next) => {
         next(error)
     }
 }
+
+export const getPosts = async (req, res, next) => {
+    try {
+        const startIndex = req.query.startIndex || 0 
+
+        const limit = req.query.limit || 9 
+
+        const order = req.query.order === "asc"? 1 : -1
+
+        const posts = await Post.find(
+            {
+                ...(req.query.userId && {userId: req.query.userId}),
+                ...(req.query.category && {category: req.query.category}),
+                ...(req.query.slug && {slug: req.query.slug}),
+                ...(req.query.postId && {_id: req.query.postId}),
+
+                // flexible search term 
+
+                ...(req.query.searchTerm && {
+                    $or:[
+                        {title: {$regex: req.query.searchTerm ,$options: "i"}},
+                        {content : {$regex: req.query.searchTerm ,$options: "i"}},
+                    ]
+                })
+
+
+            }
+        ).sort({createdAt: order}).limit(limit).skip(startIndex)
+
+        const totalPosts = await Post.countDocuments()
+
+        // get last month posts created 
+        const now =new  Date()
+        
+        const lastMonth = new Date (
+            now.getFullYear()// return cuurent year
+            ,now.getMonth() - 1 // retunr last month
+            ,now.getDate() // return current date "days"
+        ) 
+
+        const lastMonthPosts = await Post.countDocuments(
+            {createdAt: {$gte: lastMonth}}
+            )
+
+
+        res.status(200).json({
+            posts,
+            totalPosts,
+            lastMonthPosts
+        })
+
+    } catch (error) {
+        next(error)
+    }
+}
--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,9 +1,10 @@
 import express from "express"
-import {create} from "../controllers/post.controller.js"
+import {create, getPosts} from "../controllers/post.controller.js"
 import {verifyToken} from "../utlis/verifyUser.js"
 const router = express.Router() 
 
 router.post("/create-post",verifyToken,create)
 
+router.post("/get-posts",getPosts)
 
 export default router
